07398fd2036c02308716f876bf86d0ff17872982
Max Bossing <info@maxbossing.de>
2024-09-22 21:30:05 +0200

    Paper-Core(Persistent Markers)

---
+++ Paper-Core/src/main/kotlin/ng/bossi/minekraft/paper/utils/PersistentDataMarker.kt
@@ -0,0 +1,26 @@
+@file:Suppress("unused")
+package ng.bossi.minekraft.paper.utils
+
+import ng.bossi.minekraft.paper.MinekraftInstance
+import ng.bossi.minekraft.paper.items.meta
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataHolder
+import org.bukkit.persistence.PersistentDataType
+
+private fun key(key: String) = NamespacedKey(MinekraftInstance, "marker_$key")
+
+fun PersistentDataHolder.mark(key: String) =
+    persistentDataContainer.set(key(key), PersistentDataType.BYTE, 1.toByte())
+fun PersistentDataHolder.unmark(key: String) = persistentDataContainer.remove(key(key))
+fun PersistentDataHolder.isMarked(key: String) = persistentDataContainer.has(key(key), PersistentDataType.BYTE)
+
+
+fun ItemStack.mark(key: String) = meta<ItemMeta> { mark(key) }
+fun ItemStack.unmark(key: String) = meta<ItemMeta> { unmark(key) }
+fun ItemStack.isMarked(key: String): Boolean {
+    var result = false
+    meta<ItemMeta> { result = isMarked(key) }
+    return result
+}
