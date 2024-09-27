8b1ba6ef711bd0d2e6408b0c1298a9c39f6d02b6
Max Bossing <info@maxbossing.de>
2024-09-27 18:20:29 +0200

    opt into VanillaModule<T> for PaperModule

--- Paper-Core/src/main/kotlin/ng/bossi/minekraft/paper/module/PaperModule.kt
+++ Paper-Core/src/main/kotlin/ng/bossi/minekraft/paper/module/PaperModule.kt
@@ -1,6 +1,8 @@
 package ng.bossi.minekraft.paper.module
 
+import ng.bossi.minekraft.annotation.MinekraftInternal
 import ng.bossi.minekraft.module.VanillaModule
 import ng.bossi.minekraft.paper.Minekraft
 
+@OptIn(MinekraftInternal::class)
 abstract class PaperModule() : VanillaModule<Minekraft>
