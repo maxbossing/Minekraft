486413d676e4c1fba0cc4323a3b4f00400f2dd53
Max Bossing <info@maxbossing.de>
2024-10-06 15:15:09 +0200

    docs(Vanilla[Globals]): Document the global kotlinx.serialization.json and MiniMessage instance

--- Vanilla/src/main/kotlin/ng/bossi/minekraft/Globals.kt
+++ Vanilla/src/main/kotlin/ng/bossi/minekraft/Globals.kt
@@ -4,16 +4,28 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import net.kyori.adventure.text.minimessage.MiniMessage
 
+/**
+ * The global [kotlinx.serialization.json] Instance
+ *
+ * If any parts of Minekraft use [kotlinx.serialization.json], this is the instance being used
+ *
+ * To add custom behaviour, overwrite this instance with a custom one
+ */
 @OptIn(ExperimentalSerializationApi::class)
-val json by lazy {
-    Json {
-        prettyPrint = true
-        encodeDefaults = true
-        isLenient = true
-        classDiscriminator = "type"
-        prettyPrintIndent = "  "
-        ignoreUnknownKeys = true
-    }
+var json = Json {
+    prettyPrint = true
+    encodeDefaults = true
+    isLenient = true
+    classDiscriminator = "type"
+    prettyPrintIndent = "  "
+    ignoreUnknownKeys = true
 }
 
+/**
+ * The global [MiniMessage] instance
+ *
+ * If any parts of Minekraft (ex: [ng.bossi.minekraft.text.miniMessage]) use MiniMessage, this is the instance being used
+ *
+ * To add custom behavior (eg tag resolvers), overwrite this instance with a custom one
+ */
 var miniMessage = MiniMessage.miniMessage()
