e6ecb268e80bdb1dffa2e584f8632ec5558de651
Max Bossing <info@maxbossing.de>
2024-10-06 16:01:25 +0200

    docs(Vanilla[VanillaModule]): Document usage and function of VanillaModule and the module system

--- Vanilla/src/main/kotlin/ng/bossi/minekraft/module/VanillaModule.kt
+++ Vanilla/src/main/kotlin/ng/bossi/minekraft/module/VanillaModule.kt
@@ -2,6 +2,24 @@ package ng.bossi.minekraft.module
 
 import ng.bossi.minekraft.annotation.MinekraftInternal
 
+
+/**
+ * The Entrypoint to Platform modules
+ *
+ * Every Platform defines their Module Class (paper: PaperModule, fabric: FabricModule, ...) with their given entrypoint.
+ *
+ * If a module is imported, its Entrypoint (the class implementing the respective Module class)
+ * is added to the module list in the Platforms entrypoint (Paper: the Minekraft class).
+ * The load, start and stop functions will then be called at the right times.
+ *
+ * - [load] is called at the very beginning of the startup sequence (paper: when the server first loads and the worlds
+ * for example aren't loaded yet). This should be used scarcely and most things should be done in [start]
+ * - [start] is called when everything is ready for the module to start. This should be the main function used
+ * - [stop] is called when the platform is being shut down.
+ *
+ * @author Max Bossing
+ * @since 0.0.1
+ */
 @MinekraftInternal
 interface VanillaModule<T> {
     fun load(entrypoint: T)
